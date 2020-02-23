32891afd3683a0b12622fde73d300cc57ec5a7a0
dipaksutare007 <dipaksutare007tmail.com>
2020-02-23 20:26:49 +0530

    commit

--- HospitalManagement/App_Start/BundleConfig.cs
+++ HospitalManagement/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace HospitalManagement
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/custom.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/css/custom.css"));
         }
     }
 }

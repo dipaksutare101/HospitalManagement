8d3c5832137e6b6f6dd8545bcd0a829b792a6a82
dipaksutare007 <dipaksutare007tmail.com>
2020-03-10 16:36:37 +0530

    Commit Doctor Form

---
+++ HospitalManagement/Core/Repositories/ISpecializationRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalManagement.Core.Models;
+
+namespace HospitalManagement.Core.Repositories
+{
+    public interface ISpecializationRepository
+    {
+        // Get Specialization  data
+         IEnumerable<Specialization> GetSpecialization();
+        
+    }
+}
---
+++ HospitalManagement/Persistence/Repositories/SpecializationRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HospitalManagement.Core.Models;
+using HospitalManagement.Core.Repositories;
+using System.Data.Entity;
+
+namespace HospitalManagement.Persistence.Repositories
+{
+    public class SpecializationRepository : ISpecializationRepository
+    {
+        public readonly ApplicationDbContext Context;
+        public SpecializationRepository(ApplicationDbContext context)
+        {
+            Context = context;
+        }
+        public IEnumerable<Specialization> GetSpecialization()
+        {
+                return Context.Specializations.ToList();    
+        }
+    }
+}
